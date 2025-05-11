5769cabe053a83b855bef286675346edcca2500e
Igor Rodrigues <seuemail@email.com>
2025-05-10 22:23:06 -0300

    Adicionar arquivos não rastreados

---
+++ nome-do-projeto/src/main.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from  '../src/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { createServer, proxy } from 'aws-serverless-express';
+import { Context, Handler } from 'aws-lambda';
+import { Server } from 'http';
+
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  const app = await NestFactory.create(AppModule);
+
+  const apiPath = 'api';
+  app.setGlobalPrefix(apiPath);
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Exemplo de API com Swagger no NestJS')
+    .setDescription('Descrição da API de exemplo utilizando Swagger')
+    .setVersion('1.0')
+    .build();
+
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return createServer(expressApp);
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context
+) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrapServer();
+  }
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
